3f02e73c12c37fe62b4c0a38468bfdc4ccdda6f2
unknown <jurajdedic@gmail.com>
2021-09-09 13:50:07 +0200

    dist/ now included in git for deployment

---
+++ front-end/dist/precache-manifest.15afd2ed89513af26f810e5d492f8239.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8aba122f9f330e06f0",
+    "url": "/css/app.4ea16a4a.css"
+  },
+  {
+    "revision": "2e297b7b8c5ce1b78a2e951f521f7c6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f955dd37a897daeee20",
+    "url": "/js/about.45400c5e.js"
+  },
+  {
+    "revision": "bd8aba122f9f330e06f0",
+    "url": "/js/app.40f78810.js"
+  },
+  {
+    "revision": "658e59f18f7e90944cb9",
+    "url": "/js/chunk-vendors.5c078a31.js"
+  },
+  {
+    "revision": "6c5db8b1a356e1e20012a805e85a7ed8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
